4e7ab269ee968eb21789400ce3786c2cca6fe863
changhwan <chang5hustle@gmail.com>
2025-01-31 13:39:12 +0900

    너무 크게 만들어서 액티비티 분리

---
+++ ComposeLab/app/src/main/java/com/example/composelab/MainActivity2.kt
@@ -0,0 +1,30 @@
+package com.example.composelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composelab.ui.theme.ComposeLabTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeLabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                   
+                }
+            }
+        }
+    }
+}
+
+
